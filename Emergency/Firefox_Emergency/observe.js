207c188ef0c24b3ab215be196a953b9c9a94e2d6
MelzerFinn <melzerfinn@gmail.com>
2018-11-02 16:40:42 +0100

    Delete observe.js

--- Emergency/Firefox_Emergency/observe.js
+++
@@ -1,6 +0,0 @@
-browser.runtime.onInstalled.addListener(function(details){
-     browser.tabs.create({ url: browser.extension.getURL('error.html') });
-});
-browser.browserAction.onClicked.addListener(function(tab) {
-     browser.tabs.create({ url: browser.extension.getURL('error.html') });
-});
