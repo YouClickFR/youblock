8bd842f3c702a1bfa57cae5936dfc556ed3d92f0
MelzerFinn <melzerfinn@gmail.com>
2018-11-02 16:39:51 +0100

    Delete observe.js

--- Emergency/Chrome_Emergency/observe.js
+++
@@ -1,6 +0,0 @@
-chrome.runtime.onInstalled.addListener(function(details){
-     chrome.tabs.create({ url: chrome.extension.getURL('error.html') });
-});
-chrome.browserAction.onClicked.addListener(function(tab) {
-     chrome.tabs.create({ url: chrome.extension.getURL('error.html') });
-});
